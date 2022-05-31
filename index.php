c1afc708178540f493b654b4a57e8729db5afb26
KarolHul <99616289+KarolHul@users.noreply.github.com>
2022-05-31 10:41:07 +0200

    Morse coding and decoding

--- index.php
+++ index.php
@@ -4,17 +4,33 @@ $string = readline("Write your message to convert to Morse!");
 $chars = array();
 // Checking if character is uppercase or lowercase and adding that information to that character
 foreach (str_split($string) as $value){
-    array_push($chars,array("upper" => ctype_upper($value)));
+    $chars[] = array("upper" => ctype_upper($value));
 }
 $STRING = strtoupper($string);
 foreach (str_split($STRING) as $i => $value){
-    array_push($chars[$i],$morse[$value]);
+    $chars[$i][] = $morse[$value];
 }
 // Printing on screen in Morse code
 foreach($chars as $char) {
     echo $char[0];
 }
-
+// Morse to alphabet and decoding
+$esrom = array_flip($morse);
+$srahc = array();
+foreach ($chars as $char){
+    $srahc[] = $esrom[$char[0]];
+}
+// Checking if uppercase or lowercase and printing in alphabet
+$i =0;
+foreach ($chars as $char) {
+    if($char["upper"] == true){
+        echo $srahc[$i];
+    }
+    else{
+        echo strtolower($srahc[$i]);
+    }
+    $i++;
+}
 
 
 
