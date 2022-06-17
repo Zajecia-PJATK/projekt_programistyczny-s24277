a8e2955bc0f2b1a59714130f35fb74e493dbcf01
KarolHul <99616289+KarolHul@users.noreply.github.com>
2022-06-17 13:27:11 +0200

    Organized code and divided Morse to functions

--- Morse.php
+++ Morse.php
@@ -38,3 +38,61 @@ $morse = array(
     '0' => "-----",
     ' ' => "   ",
 );
+function codeMorse($string)
+{
+    global $morse;
+    $chars = array();
+// Checking if character is uppercase or lowercase and adding that information to that character
+    foreach (str_split($string) as $value) {
+        $chars[] = array("upper" => ctype_upper($value));
+    }
+    $STRING = strtoupper($string);
+    foreach (str_split($STRING) as $i => $value) {
+        $chars[$i][] = $morse[$value];
+    }
+// Printing on screen in Morse code
+    foreach ($chars as $char) {
+        echo $char[0] . ' ';
+    }
+}
+
+
+// Morse to alphabet
+$esrom = array_flip($morse);
+
+
+function decodingMorse($chars)
+{
+    global $esrom;
+
+// Decoding my morse
+    $srahc = array();
+    foreach ($chars as $char) {
+        $srahc[] = $esrom[$char[0]];
+    }
+// Checking if uppercase or lowercase and printing in alphabet
+$i =0;
+foreach ($chars as $char) {
+    if($char["upper"] == true){
+        echo $srahc[$i];
+    }
+    else{
+        echo strtolower($srahc[$i]);
+    }
+    $i++;
+}
+}
+function DecodingNMorse($input)
+{
+    global $esrom;
+//Decoding normal morse
+    $decode = explode(' ', preg_replace('/ {5}/', ' | ', $input));
+    foreach ($decode as $value) {
+        if ($value == "|") {
+            echo " ";
+        } else {
+            echo $esrom[$value];
+        }
+
+    }
+}
--- index.php
+++ index.php
@@ -1,69 +1,22 @@
+<!DOCTYPE html>
+<html>
+<style>
+    body{
+        background-image: url('zCRSn1.gif');
+        background-size: cover;
+        height: 100vh;
+        padding:0;
+        margin:0;
+    }
+</style>
+<body>
+
+</body>
+</html>
 <?php
 require 'Morse.php';
-$string = readline("Write your message to convert to Morse!");
-$chars = array();
-// Checking if character is uppercase or lowercase and adding that information to that character
-foreach (str_split($string) as $value){
-    $chars[] = array("upper" => ctype_upper($value));
-}
-$STRING = strtoupper($string);
-foreach (str_split($STRING) as $i => $value){
-    $chars[$i][] = $morse[$value];
-}
-// Printing on screen in Morse code
-foreach($chars as $char) {
-    echo $char[0] . ' ';
-}
-// Morse to alphabet and decoding
-$esrom = array_flip($morse);
-$srahc = array();
-foreach ($chars as $char){
-    $srahc[] = $esrom[$char[0]];
-}
-// Checking if uppercase or lowercase and printing in alphabet
-$i =0;
-foreach ($chars as $char) {
-    if($char["upper"] == true){
-        echo $srahc[$i];
-    }
-    else{
-        echo strtolower($srahc[$i]);
-    }
-    $i++;
-}
-
-$decode = explode(' ',preg_replace('/ {5}/',' | ',readline()));
-var_dump($decode);
-foreach ($decode as $value){
-    if($value == "|"){
-        echo " ";
-    }
-    else {
-        echo $esrom[$value];
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$example = codeMorse("Karol");
+decodingMorse($example);
+DecodingNMorse("-.- .- .-. --- .-..");
 
 ?>
